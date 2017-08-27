00a3a89f11d2938ae7134fe1f89429c6dd582550
SampadaBhagat <SampadaBhagat@users.noreply.github.com>
2017-08-27 15:59:02 +0530

    Added files via upload
    Initial upload

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularFoghornApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularFoghornApp
+ */
+angular.module('angularFoghornApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
